b00c7923630e17b984012362e0b67a89bf2f91df
Jonas Keßler <jonas.kessler@acando.de>
2017-09-12 16:20:45 +0200

    fix request mapping in rest.json.DataRowController

--- spring-boot-2-demo/src/main/java/de/acando/jk/bootdemo/newfeaturesin2/rest/json/DataRowController.java
+++ spring-boot-2-demo/src/main/java/de/acando/jk/bootdemo/newfeaturesin2/rest/json/DataRowController.java
@@ -6,13 +6,15 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
  * Spring-boot v2.0.0 comes with a new spring-boot-starter-json, which includes some dependencies
  * which we always has do declare, e.g. jackson-databind and jackson-datatype-jsr310 for convenient handling of Java 8 date types.
  */
-@RestController("/datarows")
+@RestController
+@RequestMapping("/datarows")
 public class DataRowController {
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
