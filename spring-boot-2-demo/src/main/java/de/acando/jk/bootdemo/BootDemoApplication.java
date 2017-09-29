1ab7a28605777f5f53182cacfa1e0bdce3bd3936
Jonas Keßler <jonas.kessler@acando.de>
2017-09-29 17:44:08 +0200

    add spring-security

--- spring-boot-2-demo/src/main/java/de/acando/jk/bootdemo/BootDemoApplication.java
+++ spring-boot-2-demo/src/main/java/de/acando/jk/bootdemo/BootDemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.actuate.autoconfigure.endpoint.condition.Conditi
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import de.acando.jk.bootdemo.newfeaturesin2.actuator.PersonActuatorEndpoint;
 import de.acando.jk.bootdemo.newfeaturesin2.properties.DemoProperties;
@@ -21,5 +24,21 @@ public class BootDemoApplication {
 	@ConditionalOnEnabledEndpoint
 	PersonActuatorEndpoint personEndpoint() {
 		return new PersonActuatorEndpoint();
+	}
+
+	/*
+	 * TODO: add documentation for new security features to reamde
+	 *
+	 * - security autoconfig completely disabled if custom configuration available
+	 *
+	 */
+	@Configuration
+	public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest()
+					.permitAll();
+		}
 	}
 }
